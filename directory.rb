4e53268170d576dacad5c58f98b53483e747cb8f
BarriF13 <info@bdesignstudio.co.uk>
2020-01-11 14:01:00 +0000

    Adding Hashes

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
-# adding an array for students
+# adding an array for hashes of students
 
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Rathed",
-"Michael Corleone",
-"Alex Delarge",
-"The wicked witch of the west",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
+{name: "Dr. Hannibal Lecter", cohort: :novermber},
+{name:"Darth Vader", cohort: :novermber},
+{name:"Nurse Rathed", cohort: :novermber},
+{name:"Michael Corleone", cohort: :novermber},
+{name:"Alex Delarge", cohort: :novermber},
+{name:"The wicked witch of the west", cohort: :novermber},
+{name:"Terminator", cohort: :novermber},
+{name:"Freddy Krueger", cohort: :novermber},
+{name:"The Joker", cohort: :novermber},
+{name:"Joffrey Baratheon", cohort: :novermber},
+{name:"Norman Bates", cohort: :novermber}
 ]
 # making a header
 def print_header
@@ -19,12 +19,13 @@ def print_header
   puts "--------------------------------"
 end
 # making the list (body)
-def print_list(names)
-  names.each {|name|puts name}
+def print_list(students)
+  
+  students.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 # printing the total
-def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+puts "Overall, we have #{students.count} great students"
 end
 
 # calling the methods
